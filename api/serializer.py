fe34394580d32c7c3ae53394c1b7a17472fcf1f1
NCDinesh <deenez.npl1@gmail.com>
2024-06-18 16:55:12 +0545

    removed

--- api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.models import Course
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ('id','name','teachinghour', 'tutor')
